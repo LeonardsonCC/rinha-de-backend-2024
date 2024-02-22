81c48ec415fbce66cf730e0aa5a3f5f4829c2244
LeonardsonCC <leonardson.carvalho@gmail.com>
2024-02-22 11:14:38 -0300

    using pgx batch operation to balance

--- internal/repository/balance.go
+++ internal/repository/balance.go
@@ -8,12 +8,18 @@ import (
 	"github.com/LeonardsonCC/rinha-de-backend-2024/internal/api/contracts"
 	"github.com/LeonardsonCC/rinha-de-backend-2024/internal/errs"
 	"github.com/LeonardsonCC/rinha-de-backend-2024/internal/repository/db"
+	"github.com/jackc/pgx/v5"
 )
 
 func GetBalance(c context.Context, clientID int) (*contracts.Balance, error) {
 	db, _ := db.GetConnection()
 
-	rows := db.QueryRow(c, "SELECT c.saldo, c.limite FROM clientes c WHERE id = $1", clientID)
+	b := new(pgx.Batch)
+
+	b.Queue("SELECT c.saldo, c.limite FROM clientes c WHERE id = $1", clientID)
+	b.Queue("SELECT t.valor, t.tipo, t.descricao, t.realizada_em FROM transacoes t WHERE cliente_id=$1 ORDER BY realizada_em DESC LIMIT 10", clientID)
+
+	results := db.SendBatch(c, b)
 
 	balance := &contracts.Balance{
 		Balance: contracts.BalanceData{
@@ -22,12 +28,13 @@ func GetBalance(c context.Context, clientID int) (*contracts.Balance, error) {
 		LastTransactions: make([]*contracts.Transaction, 0, 10),
 	}
 
+	rows := results.QueryRow()
 	err := rows.Scan(&balance.Balance.Total, &balance.Balance.Limit)
 	if err != nil {
 		return nil, errs.ErrAccountNotFound
 	}
 
-	txRows, err := db.Query(c, "SELECT t.valor, t.tipo, t.descricao, t.realizada_em FROM transacoes t WHERE cliente_id=$1 ORDER BY realizada_em DESC LIMIT 10", clientID)
+	txRows, err := results.Query()
 	if err != nil {
 		return nil, fmt.Errorf("failed to get last transactions: %s", err)
 	}
@@ -38,6 +45,8 @@ func GetBalance(c context.Context, clientID int) (*contracts.Balance, error) {
 		balance.LastTransactions = append(balance.LastTransactions, tx)
 	}
 	txRows.Close()
+
+	results.Close()
 
 	return balance, nil
 }
